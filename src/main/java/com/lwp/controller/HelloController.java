d2925c8f79698f0e5d886aaa943fbb5b4df1850d
lwpnbdys <lwpnbdys@163.com>
2020-06-28 23:59:45 +0800

    Update HelloController.java

--- src/main/java/com/lwp/controller/HelloController.java
+++ src/main/java/com/lwp/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    //员工2又想整个活
     @RequestMapping("hello")
     @ResponseBody
     public String hello(){
@@ -15,4 +16,5 @@ public class HelloController {
    //多整了一句话
 
     //员工1想说一句话
+    
 }
